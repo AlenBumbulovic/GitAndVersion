a1b6fd41b793a6d4882e88353c7a80d6ff2d904b
Alen Bumbulovic <alen.bumbulovic@BitCamps-Mac-mini-2.local>
2015-06-15 10:54:33 +0200

    Changes

--- src/com/bitcamp/MojaKlasa.java
+++ src/com/bitcamp/MojaKlasa.java
@@ -1,5 +1,19 @@
 package com.bitcamp;
 
 public class MojaKlasa {
+	
+	public String name;
+	public Integer id;
+	public Integer ssn;
+	
+	
+	public MojaKlasa(String name, Integer id, Integer ssn) {
+		super();
+		this.name = name;
+		this.id = id;
+		this.ssn = ssn;
+	}
+	
+	
 
 }
