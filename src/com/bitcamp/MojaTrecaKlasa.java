d0493ef4ad7a1d8035ce385f9011db848b7a08d1
Alen Bumbulovic <alen.bumbulovic@BitCamps-Mac-mini-2.local>
2015-06-15 11:05:40 +0200

    Implemented the user model into MojaTrecaKlas

--- src/com/bitcamp/MojaTrecaKlasa.java
+++ src/com/bitcamp/MojaTrecaKlasa.java
@@ -1,5 +1,80 @@
 package com.bitcamp;
 
 public class MojaTrecaKlasa {
+	
+	private String firstName;
+	private String lastName;
+	private String country;
+	private String city;
+	private String phoneNumber;
+	private Integer age;
+	
+	
+	public MojaTrecaKlasa() {
+		super();
+	}
+
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+
+	public String getLastName() {
+		return lastName;
+	}
+
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+
+	public String getCountry() {
+		return country;
+	}
+
+
+	public void setCountry(String country) {
+		this.country = country;
+	}
+
+
+	public String getCity() {
+		return city;
+	}
+
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+
+
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+
+
+	public Integer getAge() {
+		return age;
+	}
+
+
+	public void setAge(Integer age) {
+		this.age = age;
+	}
+	
+	
+	
 
 }
